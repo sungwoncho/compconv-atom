a1428bf0bfc2dfda898d366b4849bb3eeae16209
Sung Won Cho <mikeswcho@gmail.com>
2018-06-04 18:28:52 +1000

    Initial commit

---
+++ spec/compconv-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CompconvAtomView from '../lib/compconv-atom-view';
+
+describe('CompconvAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
